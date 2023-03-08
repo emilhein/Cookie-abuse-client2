571be914c4286b6c0cc35627c268831f3815bd06
emilhein <emilhein1@gmail.com>
2023-03-08 15:32:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cookie-abuse-client2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-xmuzxg)
---
+++ src/lib/Likings.svelte
@@ -0,0 +1,34 @@
+<script>
+  let likes = []
+  const ilike = (topic) => {
+    if(!likes.includes(topic)){
+      likes = [...likes, topic]
+      
+      fetch(`https://fake-ad-server.herokuapp.com/cookie/${topic}`,{
+          credentials: 'include' 
+        })
+        .then(response => {
+            console.log('got it ', response)
+        })
+        .catch(error => {
+          console.error('Panic at the disco:', error);
+        })
+    }
+  }
+</script>
+
+<button on:click={() => ilike('cats')}>
+  I like cats 
+</button>
+<button on:click={() => ilike('dogs')}>
+  I like dogs 
+</button>
+<button on:click={() => ilike('flowers')}>
+  I like flowers 
+</button>
+<button on:click={() => ilike('Phones')}>
+  I like Phones 
+</button>
+<p>
+{likes}
+</p>
